42d28962cf0cec88024a9f994ded3aae362972fb
Guilherme Monteiro Massi <GMMASSI@der.sp>
2026-05-20 14:05:46 -0300

    up

--- src/TrezzeCloud.Notifications.Application/Consumers/PaymentProcessedConsumer.cs
+++ src/TrezzeCloud.Notifications.Application/Consumers/PaymentProcessedConsumer.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MassTransit;
+using TrezzeCloud.Contracts.Events;
 
-namespace TrezzeCloud.Notifications.Application.Consumers
+namespace TrezzeCloud.Notifications.Application.Consumers;
+
+public sealed class PaymentProcessedConsumer
+    : IConsumer<PaymentProcessedEvent>
 {
-    internal class PaymentProcessedConsumer
+    public Task Consume(
+        ConsumeContext<PaymentProcessedEvent> context)
     {
+        if (context.Message.Status != "Approved")
+            return Task.CompletedTask;
+
+        Console.WriteLine("====================================");
+        Console.WriteLine("PURCHASE CONFIRMATION EMAIL");
+        Console.WriteLine($"User: {context.Message.UserId}");
+        Console.WriteLine($"Game: {context.Message.GameId}");
+        Console.WriteLine($"Price: {context.Message.Price}");
+        Console.WriteLine("Purchase approved!");
+        Console.WriteLine("====================================");
+
+        return Task.CompletedTask;
     }
 }
